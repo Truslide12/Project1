7d109690db05edc1617e804b9ba7c37b4e7734b0
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-07-30 18:43:35 -0700

    added Kathleens first draft index.html and style.css

--- code.js
+++ code.js
@@ -3,5 +3,9 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.0522, lng: -118.576},
     zoom: 10
+    console.log("map function is created");
   });
 }
+
+initMap();
+console.log("map function is running and map should be rendered");
