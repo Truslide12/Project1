2a3c6f9a5794d488f2e99628c59ad7a3c828684f
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-07-30 19:16:50 -0700

    map functioning, requested changes are complete

--- code.js
+++ code.js
@@ -2,10 +2,12 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.0522, lng: -118.576},
-    zoom: 10
-    console.log("map function is created");
+    zoom: 8
   });
 }
 
-initMap();
-console.log("map function is running and map should be rendered");
+$(document).ready(function(){
+
+  initMap();
+
+})
